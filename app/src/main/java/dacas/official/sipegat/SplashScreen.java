1a03c4d08ef66591745fbfc33af2587935307968
Next Wznu <sam@google.com>
2020-07-20 01:32:34 +0700

    add slider introduction

--- app/src/main/java/dacas/official/sipegat/SplashScreen.java
+++ app/src/main/java/dacas/official/sipegat/SplashScreen.java
@@ -7,16 +7,21 @@ import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SplashScreen extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        auth = FirebaseAuth.getInstance();
         //menghilangkan ActionBar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
@@ -25,8 +30,17 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+//
                 finish();
+                if(auth.getInstance().getCurrentUser() == null){
+                    PrefManager prefManager = new PrefManager(getApplicationContext());
+                    prefManager.setFirstTimeLaunch(true);
+                    startActivity(new Intent(SplashScreen.this, WelcomeActivity.class));
+                    finish();
+                }else{
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
             }
         }, 5000L); //3000 L = 3 detik
     }
